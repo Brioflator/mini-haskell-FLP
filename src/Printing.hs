f6b4a481f2381c2fe79f8dcee52ad04a92859034
Brioflator <balescu.alexandru2002@gmail.com>
2023-03-13 14:42:28 +0200

    fifth commit

--- src/Printing.hs
+++ src/Printing.hs
@@ -5,8 +5,15 @@ import Exp
 import Data.List (intercalate)
 
 showVar :: Var -> String
-showVar = undefined
+showVar v = getVar v 
 
 showExp :: ComplexExp -> String
-showExp = undefined
+showExp (CX v) = showVar v
+showExp (Nat n) = show n
+showExp(CLam v cexp) = "\\" ++ showVar v ++ " -> " ++ showExp cexp
+showExp(Let v cexp1 cexp2) = "let " ++ showVar v ++ " := " ++ showExp cexp1 ++ " in " ++ showExp cexp2
+showExp(LetRec v cexp1 cexp2) = "letrec " ++ showVar v ++ " := " ++ showExp cexp1 ++ " in " ++ showExp cexp2
+showExp(List cexps) = "[" ++ intercalate ", " (map showExp cexps) ++ "]"
+showExp(CApp cexp1 cexp2) = "(" ++ showExp cexp1 ++ " " ++ showExp cexp2 ++ ")"
+
 
